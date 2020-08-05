fe47e9f9714a989033704d9e6c0939bb1f46e961
Max <max@upravitelev.de>
2020-08-05 13:41:38 +0200

    add spotifiy init

--- index.js
+++ index.js
@@ -4,7 +4,21 @@ const querystring = require('querystring')
 const express = require('express')
 const app = express()
 
+// spotify init
 const SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID
 const SPOTIFY_CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET
 const SPOTIFY_CLIENT_USERNAME = process.env.SPOTIFY_CLIENT_USERNAME
-const SPOTIFY_REDIRECT_URI = process.env.SPOTIFY_REDIRECT_URI 
+const SPOTIFY_REDIRECT_URI = process.env.SPOTIFY_REDIRECT_URI
+
+// https://www.npmjs.com/package/spotify-web-api-node
+const SpotifyWebAPI = require('spotify-web-api-node')
+
+const spotifyAPI = new SpotifyWebAPI({
+    clientID: SPOTIFY_CLIENT_ID,
+    clientSecret: SPOTIFY_CLIENT_SECRET,
+    // redirectUri: SPOTIFY_REDIRECT_URI
+})
+
+// spotifyApi.setAccessToken('<your_access_token>');
+
+const scopes = ["streaming", "user-read-birthdate", "user-read-email", "user-read-private"];
