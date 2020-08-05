40805be96da35d0c86b669c018e12e3538f9e4d5
Max <max@upravitelev.de>
2020-08-05 13:24:51 +0200

    add env vars

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ const querystring = require('querystring')
 const express = require('express')
 const app = express()
 
+const SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID
+const SPOTIFY_CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET
+const SPOTIFY_CLIENT_USERNAME = process.env.SPOTIFY_CLIENT_USERNAME
+const SPOTIFY_REDIRECT_URI = process.env.SPOTIFY_REDIRECT_URI 
