04cfec3e43af3d0c4d2081eaa39ff0dcef2633b7
Max <max@upravitelev.de>
2020-08-06 10:13:27 +0200

    add spotify redirect uri

--- index.js
+++ index.js
@@ -53,3 +53,19 @@ const authenticate = () => {
 }
 
 authenticate();
+
+
+app.get("/spotifyRedirectIrl", (req, res) => {
+    resonse.send(JSON.stringify({
+        redirectURI
+    }, null, 2))
+})
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+
+//backup .env
+// SPOTIFY_REDIRECT_URI=https://grandtheftanalysis.herokuapp.com/
