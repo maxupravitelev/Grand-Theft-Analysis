a7a711913259bcdbce725546ffa955ddfe307c6d
Max <max@upravitelev.de>
2020-08-07 16:37:02 +0200

    check if URI entered

--- public/index.js
+++ public/index.js
@@ -1,7 +1,31 @@
 //
 
+// var inputElement = document.createElement('input');
+// inputElement.type = "button"
+// inputElement.addEventListener('click', function(){
+//     gotoNode(result.name);
+// });
+
+// // ​document.body.appendChild(inputElement);​
+
+// var mi = document.createElement("input");
+// mi.setAttribute('type', 'text');
+// mi.setAttribute('value', 'default');
+
+// document.body.appendChild(mi)
+
 let urlParams = new URLSearchParams(window.location.search);
 
 let spotifyURI = urlParams.get('query')
 
 console.log(spotifyURI);
+
+if (spotifyURI === null) {
+    spotifyURI = 'No URI entered'
+}
+
+document.write(spotifyURI);
+
+const postToServer = () => {
+    console.log("sf")
+}
