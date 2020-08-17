76e4520bba034fe1f2760c53d3dfdd7a229dced3
Max Upravitelev <max@upravitelev.de>
2020-08-17 11:43:51 +0200

    add ts files

--- public/src/js/Cam.ts
+++ public/src/js/Cam.ts
@@ -1,11 +1,14 @@
 const RUN_SPEED = 5.5;
+
 var camPanX = 0.0;
 var camPanY = 0.0;
 const PLAYER_DIST_FROM_CENTER_BEFORE_CAMERA_PAN_X = p1.carX;
 const PLAYER_DIST_FROM_CENTER_BEFORE_CAMERA_PAN_Y = p1.carY;
+
 const sliderMove = () => {
     var nextX = sliderX;
     var nextY = sliderY;
+
     if (p1.keyHeld_TurnLeft) {
         nextX += -TURN_RATE * Math.PI;
     }
@@ -18,44 +21,50 @@ const sliderMove = () => {
     if (p1.keyHeld_Reverse) {
         nextY += REVERSE_POWER;
     }
+
     if (getTrackAtPixelCoord(nextX, nextY) == false) {
         sliderX = nextX;
         sliderY = nextY;
     }
-};
+}
+
 const sliderReset = () => {
     // center slider on screen
     sliderX = canvas.width / 2;
     sliderY = canvas.height / 2;
-};
+}
+
 const instantCamFollow = () => {
     camPanX = p1.carX - canvas.width / 2;
     camPanY = p1.carY - canvas.height / 2;
     // camPanX = sliderX - canvas.width/2;
     // camPanY = sliderY - canvas.height/2;
-};
+}
+
 const cameraFollow = () => {
     var cameraFocusCenterX = camPanX + canvas.width / 2;
     var cameraFocusCenterY = camPanY + canvas.height / 2;
+
     var playerDistFromCameraFocusX = Math.abs(sliderX - cameraFocusCenterX);
     var playerDistFromCameraFocusY = Math.abs(sliderY - cameraFocusCenterY);
+
     if (playerDistFromCameraFocusX > PLAYER_DIST_FROM_CENTER_BEFORE_CAMERA_PAN_X) {
         if (cameraFocusCenterX < sliderX) {
             camPanX += RUN_SPEED;
-        }
-        else {
+        } else {
             camPanX -= RUN_SPEED;
         }
     }
     if (playerDistFromCameraFocusY > PLAYER_DIST_FROM_CENTER_BEFORE_CAMERA_PAN_Y) {
         if (cameraFocusCenterY < sliderY) {
             camPanY += RUN_SPEED;
-        }
-        else {
+        } else {
             camPanY -= RUN_SPEED;
         }
     }
+
     //instantCamFollow();
+
     // this next code blocks the game from showing out of bounds
     // (this isn't required, if you don't mind seeing beyond edges)
     // if(camPanX < 0) {
@@ -72,4 +81,4 @@ const cameraFollow = () => {
     // if(camPanY > maxPanTop) {
     //   camPanY = maxPanTop;
     // }
-};
+}
---
+++ public/src/js/ImageLoading.ts
@@ -0,0 +1,51 @@
+let carPic = document.createElement("img");
+    let car2Pic = document.createElement("img");
+    let roof = document.createElement("img");
+    let roof_up = document.createElement("img");
+
+    let trackPics = [];
+
+    let picsToLoad = 0;
+
+    const countLoadedImageAndLaunchIfReady = () => {
+        picsToLoad--;
+        if (picsToLoad == 0) {                          // last image loaded
+            loadingDoneSoStartGame();
+        }
+    }
+
+    const beginLoadingImage = (imgVar, fileName) => {
+        imgVar.onload = countLoadedImageAndLaunchIfReady;
+        imgVar.src = fileName;
+    }
+
+    const loadImageForTrackCode = (trackCode, fileName) => {
+        trackPics[trackCode] = document.createElement("img");
+        beginLoadingImage(trackPics[trackCode], fileName);
+    }
+
+
+    const loadImages = () => {
+
+        let imageList = [
+            { varName: carPic, theFile: "src/images/player1.png" },
+            { varName: roof, theFile: "src/images/roof.png" },
+            { varName: roof_up, theFile: "src/images/roof_up.png" },
+
+            { trackType: TRACK_ROAD, theFile: "src/images/track_road.png" },
+            { trackType: TRACK_WALL, theFile: "src/images/track_wall.png" },
+            { trackType: TRACK_GOAL, theFile: "src/images/track_goal.png" },
+            { trackType: TRACK_TREE, theFile: "src/images/track_tree.png" },
+            { trackType: TRACK_FLAG, theFile: "src/images/track_flag.png" },
+        ];
+
+        picsToLoad = imageList.length;
+
+        for (let i = 0; i < imageList.length; i++) {
+            if (imageList[i].trackType != undefined) {
+                loadImageForTrackCode(imageList[i].trackType, imageList[i].theFile);
+            } else {
+                beginLoadingImage(imageList[i].varName, imageList[i].theFile);
+            } // end of else
+        } // end of for imageList
+    } // end of function loadImages
