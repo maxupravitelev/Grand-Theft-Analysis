09fde588325e9490e6d6959b3071bf08762f2c8b
Max Upravitelev <max@upravitelev.de>
2020-11-02 16:15:23 +0100

    add fetching street by osm id

--- controllers/maps.js
+++ controllers/maps.js
@@ -3,12 +3,10 @@ const mapRouter = require('express').Router();
 const { response } = require('../index');
 const convert = require('xml-js');
 const fs = require('fs');
-mapRouter.get('/', async (request, response) => {
-    const xmlFile = fs.readFileSync('./maps/start.xml', 'utf8');
+mapRouter.get('/', (request, response) => {
+    const xmlFile = fs.readFileSync('./maps/temp.xml', 'utf8');
     const jsonData = JSON.parse(convert.xml2json(xmlFile, { compact: true, spaces: 2 }));
-    console.log(jsonData);
-    const street = jsonData.way.find(x => x._id === '71827680');
-    console.log(street);
+    let street = jsonData.osm.way.find(x => x._attributes.id === '151372374');
     response.json(street);
 });
 module.exports = mapRouter;
--- controllers/maps.ts
+++ controllers/maps.ts
@@ -4,23 +4,20 @@ const { response } = require('../index')
 const convert = require('xml-js');
 const fs = require('fs');
 
-/* .get specific street */
-mapRouter.get('/', async (request, response) => {
+/* .get street */
+mapRouter.get('/', (request, response) => {
 
 // mapRouter.get('/:id', async (request, response) => {
 
-    const xmlFile = fs.readFileSync('./maps/start.xml', 'utf8');
+    const xmlFile = fs.readFileSync('./maps/temp.xml', 'utf8');
 
-    // parse xml file as a json object
     const jsonData = JSON.parse(convert.xml2json(xmlFile, {compact: true, spaces: 2}));
     
-    console.log(jsonData)
+    let street = jsonData.osm.way.find(x => x._attributes.id === '151372374')
 
-    const street = jsonData.way.find(x => x._id === '71827680'
-        );
-
-    console.log(street)
+    // response.json(jsonData.osm.way)
     response.json(street)
+
   })
 
 module.exports = mapRouter
