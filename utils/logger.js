f5aff437afa32c3f1f0527d39127abb82c8f6ec6
Max Upravitelev <max@upravitelev.de>
2020-11-02 13:25:59 +0100

    add utils

---
+++ utils/logger.js
@@ -0,0 +1,13 @@
+"use strict";
+const info = (...params) => {
+    if (process.env.NODE_ENV !== 'test') {
+        console.log(...params);
+    }
+};
+const error = (...params) => {
+    console.log(...params);
+};
+module.exports = {
+    info,
+    error,
+};
