dd211a5734345b70435d6cf70b35f04d2cf8f80e
Max Upravitelev <max@upravitelev.de>
2020-11-02 16:39:35 +0100

    fetch nodes in street by osm id

--- controllers/maps.js
+++ controllers/maps.js
@@ -7,6 +7,18 @@ mapRouter.get('/', (request, response) => {
     const xmlFile = fs.readFileSync('./maps/temp.xml', 'utf8');
     const jsonData = JSON.parse(convert.xml2json(xmlFile, { compact: true, spaces: 2 }));
     let street = jsonData.osm.way.find(x => x._attributes.id === '151372374');
-    response.json(street);
+    response.json(jsonData);
+});
+mapRouter.get('/nodesinstreet', (request, response) => {
+    const xmlFile = fs.readFileSync('./maps/temp.xml', 'utf8');
+    const jsonData = JSON.parse(convert.xml2json(xmlFile, { compact: true, spaces: 2 }));
+    let street = jsonData.osm.way.find(x => x._attributes.id === '151372374');
+    let nodesInStreet = [];
+    street.nd.forEach(node => nodesInStreet.push(node));
+    let nodeArr = [];
+    nodesInStreet.forEach(node => nodeArr.push(jsonData.osm.node.find(x => x._attributes.id === node._attributes.ref)));
+    let finalArr = [];
+    nodeArr.forEach(data => finalArr.push({ lat: data._attributes.lat, lon: data._attributes.lon }));
+    response.json(finalArr);
 });
 module.exports = mapRouter;
--- controllers/maps.ts
+++ controllers/maps.ts
@@ -15,9 +15,37 @@ mapRouter.get('/', (request, response) => {
     
     let street = jsonData.osm.way.find(x => x._attributes.id === '151372374')
 
+
     // response.json(jsonData.osm.way)
-    response.json(street)
+    response.json(jsonData)
 
   })
+
+mapRouter.get('/nodesinstreet', (request, response) => {
+
+    // mapRouter.get('/:id', async (request, response) => {
+    
+        const xmlFile = fs.readFileSync('./maps/temp.xml', 'utf8');
+    
+        const jsonData = JSON.parse(convert.xml2json(xmlFile, {compact: true, spaces: 2}));
+        
+        let street = jsonData.osm.way.find(x => x._attributes.id === '151372374')
+    
+        let nodesInStreet = []
+
+        street.nd.forEach(node => nodesInStreet.push(node))
+
+        let nodeArr = []
+
+        nodesInStreet.forEach(node => nodeArr.push(jsonData.osm.node.find(x => x._attributes.id === node._attributes.ref))) 
+
+        let finalArr = []
+
+        nodeArr.forEach(data => finalArr.push({lat: data._attributes.lat, lon: data._attributes.lon}))
+
+
+        response.json(finalArr)
+    
+      })
 
 module.exports = mapRouter
