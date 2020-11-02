4e32a09110e0c5d9a279320fb9d4a66814855ad5
Max Upravitelev <max@upravitelev.de>
2020-11-02 13:45:35 +0100

    add xml-js to package

--- controllers/maps.js
+++ controllers/maps.js
@@ -1,7 +1,14 @@
 "use strict";
 const mapRouter = require('express').Router();
 const { response } = require('../index');
-mapRouter.get('/:id', async (request, response) => {
-    const street = response.json(street);
+const convert = require('xml-js');
+const fs = require('fs');
+mapRouter.get('/', async (request, response) => {
+    const xmlFile = fs.readFileSync('./maps/start.xml', 'utf8');
+    const jsonData = JSON.parse(convert.xml2json(xmlFile, { compact: true, spaces: 2 }));
+    console.log(jsonData);
+    const street = jsonData.way.find(x => x._id === '71827680');
+    console.log(street);
+    response.json(street);
 });
 module.exports = mapRouter;
--- controllers/maps.ts
+++ controllers/maps.ts
@@ -1,11 +1,25 @@
 const mapRouter = require('express').Router()
 const { response } = require('../index')
 
+const convert = require('xml-js');
+const fs = require('fs');
+
 /* .get specific street */
-mapRouter.get('/:id', async (request, response) => {
+mapRouter.get('/', async (request, response) => {
 
-    const street = 
+// mapRouter.get('/:id', async (request, response) => {
 
+    const xmlFile = fs.readFileSync('./maps/start.xml', 'utf8');
+
+    // parse xml file as a json object
+    const jsonData = JSON.parse(convert.xml2json(xmlFile, {compact: true, spaces: 2}));
+    
+    console.log(jsonData)
+
+    const street = jsonData.way.find(x => x._id === '71827680'
+        );
+
+    console.log(street)
     response.json(street)
   })
 
