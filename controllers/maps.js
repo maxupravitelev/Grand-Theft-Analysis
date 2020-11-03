ea801ade0edf6a2a86b680390d97d4625d66631b
Max Upravitelev <max@upravitelev.de>
2020-11-03 16:08:24 +0100

    refactor response json @nodesinstreet

--- controllers/maps.js
+++ controllers/maps.js
@@ -10,19 +10,18 @@ mapRouter.get('/', (request, response) => {
 });
 mapRouter.get('/nodesinstreet', (request, response) => {
     let street = jsonData.osm.way.find(x => x._attributes.id === '151372374');
+    let streetName = filterByValue(street.tag, "name");
     let nodesInStreet = [];
     street.nd.forEach((node) => nodesInStreet.push(node));
     let nodeData = [];
     nodesInStreet.forEach(node => nodeData.push(jsonData.osm.node.find(x => x._attributes.id === node._attributes.ref)));
     let nodesInStreetCoordinates = [];
-    nodeData.forEach(data => nodesInStreetCoordinates.push({ lat: data._attributes.lat, lon: data._attributes.lon }));
+    nodeData.forEach(data => nodesInStreetCoordinates.push({ nodeId: data._attributes.id, streetName: streetName[0]._attributes.v, lat: data._attributes.lat, lon: data._attributes.lon }));
     response.json(nodesInStreetCoordinates);
 });
 mapRouter.get('/nextstreet', (request, response) => {
-    function filterByValue(array, value) {
-        return array.filter((data) => JSON.stringify(data).toLowerCase().indexOf(value.toLowerCase()) !== -1);
-    }
     let nextStreet = filterByValue(jsonData.osm.way, '26876446');
     response.json(nextStreet);
 });
+const filterByValue = (array, value) => array.filter((data) => JSON.stringify(data).toLowerCase().indexOf(value.toLowerCase()) !== -1);
 module.exports = mapRouter;
--- controllers/maps.ts
+++ controllers/maps.ts
@@ -20,7 +20,9 @@ mapRouter.get('/nodesinstreet', (request, response) => {
 // mapRouter.get('/:id', async (request, response) => {
     
   let street = jsonData.osm.way.find(x => x._attributes.id === '151372374')
-      
+  
+  let streetName = filterByValue(street.tag, "name")
+
   let nodesInStreet: object[] = []
   street.nd.forEach((node: object) => nodesInStreet.push(node))
 
@@ -28,23 +30,24 @@ mapRouter.get('/nodesinstreet', (request, response) => {
   nodesInStreet.forEach(node => nodeData.push(jsonData.osm.node.find(x => x._attributes.id === node._attributes.ref))) 
 
   let nodesInStreetCoordinates: object[] = []
-  nodeData.forEach(data => nodesInStreetCoordinates.push({lat: data._attributes.lat, lon: data._attributes.lon}))
+  nodeData.forEach(data => nodesInStreetCoordinates.push({nodeId: data._attributes.id, streetName: streetName[0]._attributes.v, lat: data._attributes.lat, lon: data._attributes.lon}))
 
   response.json(nodesInStreetCoordinates)
 })
 
 mapRouter.get('/nextstreet', (request, response) => {
       
-
-    // Source: https://stackoverflow.com/questions/44312924/filter-array-of-objects-whose-any-properties-contains-a-value
-    function filterByValue(array, value) {
-      return array.filter((data) =>  JSON.stringify(data).toLowerCase().indexOf(value.toLowerCase()) !== -1);
-    }
-
+    // find street by node id
     let nextStreet = filterByValue(jsonData.osm.way, '26876446')
 
-  
     response.json(nextStreet)
   })
 
+/* HELPER FUNCTIONS*/
+
+// Source: https://stackoverflow.com/questions/44312924/filter-array-of-objects-whose-any-properties-contains-a-value
+const filterByValue = (array: object[], value: string) => array.filter((data) =>  JSON.stringify(data).toLowerCase().indexOf(value.toLowerCase()) !== -1);
+
+
+  
 module.exports = mapRouter
